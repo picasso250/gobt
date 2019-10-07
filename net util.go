68dc12739fb054a2bf2ed3412bbba06a6756e5b2
xiaochi <cumt.xiaochi@gmail.com>
2019-10-07 22:12:44 +0800

    writeIntegers

--- net util.go
+++ net util.go
@@ -1,6 +1,7 @@
 package gobt
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 	"log"
@@ -11,7 +12,17 @@ func writeInteger(w io.Writer, i interface{}) error {
 	// All later integers sent in the protocol are encoded as four bytes big-endian.
 	return binary.Write(w, binary.BigEndian, i)
 }
-
+func writeIntegers(w io.Writer, is ...interface{}) error {
+	// All later integers sent in the protocol are encoded as four bytes big-endian.
+	buf := new(bytes.Buffer)
+	for _, v := range is {
+		err := binary.Write(buf, binary.BigEndian, v)
+		if err != nil {
+			return err
+		}
+	}
+	return writeAll(w, buf.Bytes())
+}
 func readUint32(conn io.Reader) (uint32, error) {
 	var i uint32
 	err := binary.Read(conn, binary.BigEndian, &i)
