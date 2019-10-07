5314ad6f1f20b0ae5b512d07ae9babe0efd3c5c3
xiaochi <cumt.xiaochi@gmail.com>
2019-10-07 19:13:49 +0800

    z

--- client_test.go
+++ client_test.go
@@ -1,12 +1,10 @@
 package gobt
 
 import (
-	"log"
 	"testing"
 )
 
 func TestDownload(t *testing.T) {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	Download("Mutant.Year.Zero.Road.to.Eden.Seed.of.Evil.torrent")
 	t.Errorf("not implemented")
 }
