a261737c6045068c570c5ab621f8530510ba7a69
xiaochi <cumt.xiaochi@gmail.com>
2019-10-07 20:48:46 +0800

    seek

--- client/client.go
+++ client/client.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"github.com/picasso250/gobt"
+	"os"
 )
 
 func main() {
-	i, err := gobt.Parse([]byte("i11e"))
+	f, err := os.OpenFile("z", os.O_RDWR|os.O_CREATE, 0664)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("hello %d,%d\n", i, uint32(int32(-2)))
+	o, err := f.Seek(188, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("new offset: %d\n", o)
+	n, err := f.Write([]byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("write: %d\n", n)
 }
