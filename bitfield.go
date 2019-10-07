19b2e29f6b3e703da7de7b303dce42bfd0d80e51
xiaochi <cumt.xiaochi@gmail.com>
2019-10-07 13:24:54 +0800

    z

---
+++ bitfield.go
@@ -0,0 +1,22 @@
+package gobt
+
+import "log"
+
+type bitfield []byte
+
+func (b bitfield) Bit(i int) int {
+	bi := i / 8
+	ii := i % 8
+	bv := b[bi]
+	bv >>= (7 - ii)
+	return int(bv) & 1
+}
+func (b bitfield) SetBit(i int, v byte) {
+	if v|1 != v {
+		log.Fatal("bit must be 0 or 1")
+	}
+	bi := i / 8
+	ii := i % 8
+	v <<= (7 - ii)
+	b[bi] |= v
+}
